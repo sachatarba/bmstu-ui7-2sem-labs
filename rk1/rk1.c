38a9e8c8367322c09da7664cc4bd3851af8656c1
AndreyRazin <razin.andrew.ru@mail.ru>
2022-04-04 11:14:15 +0300

    Добавлено решение и тестовые файлы для РК1

---
+++ rk1/rk1.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+
+int input_array(int *array, int *size);
+
+void modify_array(int *array, int size);
+
+void print_array(int *array, int size);
+
+int main(void)
+{
+	int size = 0;
+	int array[10] = { 0 };
+	input_array(array, &size);
+	modify_array(array, size);
+	print_array(array, size);
+	
+	return 0;
+}
+
+int input_array(int *array, int *size)
+{
+	int rc = scanf("%d", size);
+	
+	if (rc)
+	{
+		for (int i = 0; i < *size; ++i)
+		{
+			rc = scanf("%d", array + i);
+		}
+	
+	}
+	
+	return rc;
+}
+
+void print_array(int *array, int size)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		printf("%d ", array[i]);
+	}
+}
+
+void modify_array(int *array, int size)
+{
+	int array_null[10];
+	int array_neg[10];
+	int array_pos[10];
+	
+	int c_null = 0;
+	int c_neg = 0;
+	int c_pos = 0;
+	
+	for (int i = 0; i < size; ++i)
+	{
+		if (array[i] == 0)
+		{
+			array_null[c_null] = array[i];
+			++c_null;
+		}
+		
+		if (array[i] < 0)
+		{
+			array_neg[c_neg] = array[i];
+			++c_neg;
+		}
+		
+		if (array[i] > 0)
+		{
+			array_pos[c_pos] = array[i];
+			++c_pos;
+		}
+	}	
+		
+	int c = 0;
+		
+	for (int i = 0; i < c_null; ++i)
+	{
+		array[c] = array_null[i];
+		++c;
+	}
+		
+	for (int i = 0; i < c_neg; ++i)
+	{
+		array[c] = array_neg[i];
+		++c;
+	}
+		
+	for (int i = 0; i < c_pos; ++i)
+	{
+		array[c] = array_pos[i];
+		++c;
+	}
+}
+
