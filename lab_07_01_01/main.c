84c6ea9f40bdf5d749991719e2014b4fd2529fc0
Alexander Tarba <tav21u332@student.bmstu.ru>
2022-09-29 18:34:37 +0300

    fixed code styel

--- lab_07_01_01/main.c
+++ lab_07_01_01/main.c
@@ -59,7 +59,6 @@ int main(int argc, char **argv)
 
                         fclose(fout);
                     }
-
                 }
             }
             else
--- lab_07_01_01/process.c
+++ lab_07_01_01/process.c
@@ -13,7 +13,7 @@ int key(const int *pb_src, const int *pe_src, int **pb_dst, int **pe_dst)
     size_t max_ind = find_index_of_max(pb_src, pe_src);
 
     size_t start_ind = min_ind < max_ind ? min_ind + 1 : max_ind + 1;
-    size_t end_ind = min_ind > max_ind ? min_ind: max_ind;
+    size_t end_ind = min_ind > max_ind ? min_ind : max_ind;
     size_t len = end_ind - start_ind;
 
     if (len > 0)
